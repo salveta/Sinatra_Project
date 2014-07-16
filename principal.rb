cdf7b562e4aa3f1222684a40cb32bc4118e5b90d
salveta <salveta4_@hotmail.com>
2014-07-16 14:35:19 +0200

    First basic tarject generator implemented

--- principal.rb
+++ principal.rb
@@ -1,13 +1,19 @@
-require 'rubygems'
 require 'sinatra'
 
+  get '/' do
+    erb :form
+  end
 
-	get '/' do
-		erb :form
-	end
+  post '/form' do
+    erb :template, :locals => {:params => params}
+  end
 
-	post '/' do
-		params[:form]
-	end
+  post '/templates' do
+    display = params[:template]
+    erb display.to_sym, :locals => {:params => params}
+  end
 
-
+  get '/:template/:eventname/:eventhour/:place/:description/:name' do
+    display = params[:template]
+    erb display.to_sym, :locals => {:params => params}
+  end
